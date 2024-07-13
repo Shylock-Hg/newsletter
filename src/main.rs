95ec27cfeefb732421a8c29dbd51af0bc169f07e
Shylock Hg <tcath2s@icloud.com>
2024-07-13 09:37:12 +0800

    Add health check.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("world");
     format!("Hello {}.", name)
+}
+
+async fn health_check(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().finish()
 }
 
 #[tokio::main]
@@ -10,6 +14,8 @@ async fn main() -> Result<(), std::io::Error> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
+            // match this first instead of greet
+            .route("/health_check", web::get().to(health_check))
             .route("/{name}", web::get().to(greet))
     })
     .bind("127.0.0.1:8080")?
